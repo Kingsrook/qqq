d359e7ca13afa0d10c48f4cf6c49430d72d3433b
Darin Kelkhoff <dkelkhoff@infopluscommerce.com>
2021-11-28 21:39:56 -0600

    Add most (all?) operators

--- src/main/java/com/kingsrook/qqq/backend/core/model/actions/QCriteriaOperator.java
+++ src/main/java/com/kingsrook/qqq/backend/core/model/actions/QCriteriaOperator.java
@@ -8,5 +8,18 @@ public enum QCriteriaOperator
 {
    EQUALS,
    NOT_EQUALS,
-   IN
+   IN,
+   NOT_IN,
+   STARTS_WITH,
+   ENDS_WITH,
+   CONTAINS,
+   NOT_STARTS_WITH,
+   NOT_ENDS_WITH,
+   NOT_CONTAINS,
+   LESS_THAN,
+   LESS_THAN_OR_EQUALS,
+   GREATER_THAN,
+   GREATER_THAN_OR_EQUALS,
+   IS_BLANK,
+   IS_NOT_BLANK
 }
