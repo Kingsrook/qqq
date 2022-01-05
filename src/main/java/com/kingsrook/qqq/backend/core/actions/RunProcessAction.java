b5e11176f61377eceef0851945cf34872f0b0f97
Darin Kelkhoff <darin.kelkhoff@kingsrook.com>
2022-01-04 18:15:39 -0600

    Checkpoint

--- src/main/java/com/kingsrook/qqq/backend/core/actions/RunProcessAction.java
+++ src/main/java/com/kingsrook/qqq/backend/core/actions/RunProcessAction.java
@@ -5,24 +5,14 @@
 package com.kingsrook.qqq.backend.core.actions;
 
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import com.kingsrook.qqq.backend.core.exceptions.QException;
-import com.kingsrook.qqq.backend.core.interfaces.FunctionBody;
 import com.kingsrook.qqq.backend.core.model.actions.processes.RunFunctionRequest;
 import com.kingsrook.qqq.backend.core.model.actions.processes.RunFunctionResult;
 import com.kingsrook.qqq.backend.core.model.actions.processes.RunProcessRequest;
 import com.kingsrook.qqq.backend.core.model.actions.processes.RunProcessResult;
-import com.kingsrook.qqq.backend.core.model.actions.query.QueryRequest;
-import com.kingsrook.qqq.backend.core.model.actions.query.QueryResult;
-import com.kingsrook.qqq.backend.core.model.metadata.QCodeReference;
-import com.kingsrook.qqq.backend.core.model.metadata.QFieldMetaData;
-import com.kingsrook.qqq.backend.core.model.metadata.processes.QFunctionInputMetaData;
 import com.kingsrook.qqq.backend.core.model.metadata.processes.QFunctionMetaData;
 import com.kingsrook.qqq.backend.core.model.metadata.processes.QProcessMetaData;
-import com.kingsrook.qqq.backend.core.model.metadata.processes.QRecordListMetaData;
 
 
 /*******************************************************************************
@@ -46,23 +36,25 @@ public class RunProcessAction
          throw new QException("Process [" + runProcessRequest.getProcessName() + "] is not defined in this instance.");
       }
 
-      RunProcessResult runProcessResult = runProcessFunctions(process);
+      RunProcessResult runProcessResult = new RunProcessResult();
+
+      // todo - custom routing?
+      List<QFunctionMetaData> functionList = process.getFunctionList();
+      for(QFunctionMetaData function : functionList)
+      {
+         RunFunctionRequest runFunctionRequest = new RunFunctionRequest(runProcessRequest.getInstance());
+         runFunctionRequest.setProcessName(process.getName());
+         runFunctionRequest.setFunctionName(function.getName());
+         // todo - how does this work again? runFunctionRequest.setCallback(?);
+         RunFunctionResult functionResult = new RunFunctionAction().execute(runFunctionRequest);
+         if(functionResult.getError() != null)
+         {
+            runProcessResult.setError(functionResult.getError());
+            break;
+         }
+      }
 
       return (runProcessResult);
-   }
-
-
-
-   /*******************************************************************************
-    **
-    *******************************************************************************/
-   private RunProcessResult runProcessFunctions(QProcessMetaData process)
-   {
-      List<QFunctionMetaData> functionList = process.getFunctionList();
-      for(QFunctionMetaData qFunctionMetaData : functionList)
-      {
-         // todo - custom routing?
-      }
    }
 
 }
