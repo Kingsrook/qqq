a556bf97647ac4ab59457ffa382cffe3dde3715b
Tim Chamberlain <tchamberlain76@gmail.com>
2023-01-12 20:16:06 -0600

    SPRINT-18: added slack middleware module

--- qqq-dev-tools/bin/resolve-pom-conflicts.sh
+++ qqq-dev-tools/bin/resolve-pom-conflicts.sh
@@ -4,5 +4,4 @@
 ## resolve-pom-conflicts.sh
 ## Tries to automatically resove pom conflicts by putting SNAPSHOT back
 ############################################################################
-gsed "/Updated upstream/,/=======/d" pom.xml | gsed "/Stashed/d" > /tmp/temp-pom.xml
-mv /tmp/temp-pom.xml pom.xml
+gsed -i "/Updated upstream/,/=======/d;/Stashed/d" pom.xml
