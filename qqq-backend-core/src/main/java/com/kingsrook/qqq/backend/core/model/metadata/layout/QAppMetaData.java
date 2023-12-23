940c5ca8de5d3fbc82a7d735a0ada30ec03f5833
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2023-12-22 19:09:17 -0600

    Overload withSectionOfChildren that takes Collection instead of varargs

--- qqq-backend-core/src/main/java/com/kingsrook/qqq/backend/core/model/metadata/layout/QAppMetaData.java
+++ qqq-backend-core/src/main/java/com/kingsrook/qqq/backend/core/model/metadata/layout/QAppMetaData.java
@@ -23,6 +23,8 @@ package com.kingsrook.qqq.backend.core.model.metadata.layout;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import com.kingsrook.qqq.backend.core.model.metadata.QInstance;
 import com.kingsrook.qqq.backend.core.model.metadata.TopLevelMetaDataInterface;
@@ -357,11 +359,11 @@ public class QAppMetaData implements QAppChildMetaData, MetaDataWithPermissionRu
    /*******************************************************************************
     **
     *******************************************************************************/
-   public QAppMetaData withSectionOfChildren(QAppSection section, QAppChildMetaData... children)
+   public QAppMetaData withSectionOfChildren(QAppSection section, Collection<? extends QAppChildMetaData> children)
    {
       this.addSection(section);
 
-      for(QAppChildMetaData child : children)
+      for(QAppChildMetaData child : CollectionUtils.nonNullCollection(children))
       {
          withChild(child);
          if(child instanceof QTableMetaData)
@@ -383,6 +385,15 @@ public class QAppMetaData implements QAppChildMetaData, MetaDataWithPermissionRu
       }
 
       return (this);
+   }
+
+
+   /*******************************************************************************
+    **
+    *******************************************************************************/
+   public QAppMetaData withSectionOfChildren(QAppSection section, QAppChildMetaData... children)
+   {
+      return (withSectionOfChildren(section, children == null ? null : Arrays.stream(children).toList()));
    }
 
 
