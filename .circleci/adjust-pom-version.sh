8f334c9a53998b24226b2bbe9382f398e2175264
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2024-08-22 09:45:22 -0500

    Fix syntax

--- .circleci/adjust-pom-version.sh
+++ .circleci/adjust-pom-version.sh
@@ -8,7 +8,7 @@ fi
 if [ "$CIRCLE_BRANCH" == "dev" ] || [ "$CIRCLE_BRANCH" == "staging" ] || [ "$CIRCLE_BRANCH" == "main" ] || [ \! -z $(echo "$CIRCLE_TAG" | grep "^version-") ]; then
    echo "On a primary branch or tag [${CIRCLE_BRANCH}${CIRCLE_TAG}] - will not edit the pom version.";
    echo "(or do we need to do this for dev...?)"
-   echo "(maybe we do this if the current version is a SNAPSHOT?")"
+   echo "(maybe we do this if the current version is a SNAPSHOT?)"
    exit 0;
 fi
 
