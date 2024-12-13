ec74649c96e41b8cac89e390c27afe2312c48556
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2024-12-13 11:23:38 -0600

    Introduce annotations that can be found by MetaDataProducerHelper, to make more meta-data, with less code.  Specifically: - PVS from PossibleValueEnum - PVS from RecordEntity - Joins from a parent-entity to child-entities - ChildRecordList Widgets from a parent-entity to child-entities

---
+++ qqq-backend-core/src/main/java/com/kingsrook/qqq/backend/core/model/metadata/producers/PossibleValueSourceOfEnumGenericMetaDataProducer.java
@@ -0,0 +1,64 @@
+/*
+ * QQQ - Low-code Application Framework for Engineers.
+ * Copyright (C) 2021-2024.  Kingsrook, LLC
+ * 651 N Broad St Ste 205 # 6917 | Middletown DE 19709 | United States
+ * contact@kingsrook.com
+ * https://github.com/Kingsrook/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.kingsrook.qqq.backend.core.model.metadata.producers;
+
+
+import com.kingsrook.qqq.backend.core.model.metadata.MetaDataProducerInterface;
+import com.kingsrook.qqq.backend.core.model.metadata.QInstance;
+import com.kingsrook.qqq.backend.core.model.metadata.possiblevalues.PossibleValueEnum;
+import com.kingsrook.qqq.backend.core.model.metadata.possiblevalues.QPossibleValueSource;
+
+
+/***************************************************************************
+ ** Generic meta-data-producer, which should be instantiated (e.g., by
+ ** MetaDataProducer Helper), to produce a QPossibleValueSource meta-data
+ ** based on a PossibleValueEnum
+ **
+ ***************************************************************************/
+public class PossibleValueSourceOfEnumGenericMetaDataProducer<T extends PossibleValueEnum<T>> implements MetaDataProducerInterface<QPossibleValueSource>
+{
+   private final String                 name;
+   private final PossibleValueEnum<?>[] values;
+
+
+
+   /*******************************************************************************
+    ** Constructor
+    **
+    *******************************************************************************/
+   public PossibleValueSourceOfEnumGenericMetaDataProducer(String name, PossibleValueEnum<?>[] values)
+   {
+      this.name = name;
+      this.values = values;
+   }
+
+
+
+   /***************************************************************************
+    **
+    ***************************************************************************/
+   @Override
+   public QPossibleValueSource produce(QInstance qInstance)
+   {
+      return (QPossibleValueSource.newForEnum(name, values));
+   }
+}
