a659dc7a020f9c6a1719f811e13805ce9aef4cab
Tim Chamberlain <tchamberlain76@gmail.com>
2025-02-20 23:25:35 -0600

    CE-2261: updated call to set table name to not be escaped

--- qqq-backend-module-rdbms/src/main/java/com/kingsrook/qqq/backend/module/rdbms/actions/RDBMSInsertAction.java
+++ qqq-backend-module-rdbms/src/main/java/com/kingsrook/qqq/backend/module/rdbms/actions/RDBMSInsertAction.java
@@ -37,6 +37,7 @@ import com.kingsrook.qqq.backend.core.model.data.QRecord;
 import com.kingsrook.qqq.backend.core.model.metadata.fields.QFieldMetaData;
 import com.kingsrook.qqq.backend.core.model.metadata.tables.QTableMetaData;
 import com.kingsrook.qqq.backend.core.utils.CollectionUtils;
+import com.kingsrook.qqq.backend.core.utils.StringUtils;
 
 
 /*******************************************************************************
@@ -132,6 +133,10 @@ public class RDBMSInsertAction extends AbstractRDBMSAction implements InsertInte
                for(QRecord record : page)
                {
                   QRecord outputRecord = new QRecord(record);
+                  if(!StringUtils.hasContent(outputRecord.getTableName()))
+                  {
+                     outputRecord.setTableName(getTableName(table));
+                  }
                   outputRecords.add(outputRecord);
                }
                continue;
