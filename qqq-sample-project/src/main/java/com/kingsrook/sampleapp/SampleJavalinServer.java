8e9954c9094ddf9996f021995e39f6a2439fc5e7
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2025-03-12 20:19:07 -0500

    add a ProcessBasedRouter to the sample site, and SimpleRouteAuthenticator

--- qqq-sample-project/src/main/java/com/kingsrook/sampleapp/SampleJavalinServer.java
+++ qqq-sample-project/src/main/java/com/kingsrook/sampleapp/SampleJavalinServer.java
@@ -22,10 +22,12 @@
 package com.kingsrook.sampleapp;
 
 
-import java.util.List;
 import com.kingsrook.qqq.backend.core.logging.QLogger;
+import com.kingsrook.qqq.backend.core.model.metadata.code.QCodeReference;
 import com.kingsrook.qqq.middleware.javalin.QApplicationJavalinServer;
+import com.kingsrook.qqq.middleware.javalin.routeproviders.ProcessBasedRouter;
 import com.kingsrook.qqq.middleware.javalin.routeproviders.SimpleFileSystemDirectoryRouter;
+import com.kingsrook.qqq.middleware.javalin.routeproviders.authentication.SimpleRouteAuthenticator;
 import com.kingsrook.sampleapp.metadata.SampleMetaDataProvider;
 
 
@@ -56,7 +58,13 @@ public class SampleJavalinServer
       try
       {
          QApplicationJavalinServer javalinServer = new QApplicationJavalinServer(new SampleMetaDataProvider());
-         javalinServer.withAdditionalRouteProvider(new SimpleFileSystemDirectoryRouter("/static-site", "/Users/dkelkhoff/tmp/static-site"));
+
+         javalinServer.withAdditionalRouteProvider(new SimpleFileSystemDirectoryRouter("/static-site", "static-site/")
+            .withRouteAuthenticator(new QCodeReference(SimpleRouteAuthenticator.class)));
+
+         javalinServer.withAdditionalRouteProvider(new ProcessBasedRouter("dynamic-site/<pagePath>", "DynamicSiteProcess")
+            .withRouteAuthenticator(new QCodeReference(SimpleRouteAuthenticator.class)));
+
          javalinServer.start();
       }
       catch(Exception e)
